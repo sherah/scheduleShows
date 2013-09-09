81309175a11a12718767effe813dcc6f24121893
sherah <sherah@sherahsmith.com>
2013-09-09 12:11:58 -0700

    write files with new data

--- filehandler.js
+++ filehandler.js
@@ -3,18 +3,14 @@ var fs = require('fs');
 
 exports.fileHandler = {
   readTableRecordData: function(table, id){
-    console.log("readTableRecordData running");
     var file = ("data/" + table + '/' + id + ".json");
     var data = fs.readFileSync(file, 'utf-8');
     return data;
   },
 
   readTableData: function(table){
-    console.log("readTableData running");
     var directory = ("data/" + table + "/");
-    console.log("the directory is: " + directory);
     var filesArray = fs.readdirSync(directory);
-    console.log("the files in that directory are: " + filesArray);
     var dataArray = [];
 
     filesArray.forEach(function(el){
@@ -28,8 +24,11 @@ exports.fileHandler = {
 
   writeRecord: function(table, id, data){
     //if the id exists, replace the record with the data
-    
+    if(this.readTableRecordData(table, id)){
+      var file = ("data/" + table + '/' + id + ".json");
+      fs.writeFileSync(file, data);
+    } else {
     //otherwise add this record
-  
+    }
   }
 }
